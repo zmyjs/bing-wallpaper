a2e768487af68baf2712da398cbd4f3a93825214
zmy <ikoz@qq.com>
2019-10-30 17:03:41 +0800

    代码优化

--- functions/wallpaper.js
+++ functions/wallpaper.js
@@ -1,24 +1,40 @@
 const https = require('https');
 
-const location = {
+const api = {
     origin: 'https://cn.bing.com',
-    pathname: '/HPImageArchive.aspx',
-    search: '?format=js&idx=0&n=1&pid=hp',
-    get href() {
-        return this.origin + this.pathname + this.search;
+    pathname: '/HPImageArchive.aspx'
+};
+
+function absolute(img) {
+    const reg = /^\/\w+/;
+    for (const key in img) {
+        const value = img[key];
+        if (reg.test(value)) {
+            img[key] = api.origin + value;
+        }
     }
 }
 
 exports.handler = function (event, context, callback) {
-    https.get(location.href, function (res) {
+    const search = { n: 1, idx: 0 };
+    Object.assign(search, event.queryStringParameters);
+
+    let url = `${api.origin}${api.pathname}?`;
+    for (const key in search) {
+        url += `${key}=${search[key]}&`;
+    }
+    url += 'format=js';
+
+    https.get(url, function (res) {
         res.on('data', function (data) {
             const json = data.toString();
-
-            send(json);
+            success(JSON.parse(json));
         });
-    });
+    }).on('error', callback);
 
-    function send(json) {
+    function success(data) {
+        data.images.forEach(absolute);
+
         callback(null, {
             statusCode: 200,
             headers: {
@@ -27,7 +43,7 @@ exports.handler = function (event, context, callback) {
                 'Access-Control-Allow-Credentials': 'true',
                 'Access-Control-Allow-Headers': 'authorization'
             },
-            body: json
+            body: JSON.stringify(data)
         });
     }
 }
