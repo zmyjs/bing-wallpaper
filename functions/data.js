74cbfa444f768c5435db80880af9c9f352951dd7
zmy <ikoz@qq.com>
2019-11-01 01:57:50 +0800

    bug修复，代码优化

--- functions/data.js
+++ functions/data.js
@@ -1,6 +1,12 @@
 const https = require('https');
 const { URL } = require('url');
 
+const acaHeaders = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': 'true',
+    'Access-Control-Allow-Headers': 'authorization'
+};
+
 /**
  * 接口原版参数：
  * n 数量，必填
@@ -19,55 +25,38 @@ function getData(params, options) {
         url.searchParams.set(key, params[value]);
     }
 
-    Object.assign(url, options);
+    Object.assign(options, url);
 
-    console.log(url);
+    console.log(options);
 
     return new Promise(function (resolve, reject) {
-        https.get(url, function (res) {
+        https.get(options, function (res) {
+            const { statusCode } = res;
+            const headers = res.getHeaders();
+            Object.assign(headers, acaHeaders);
+
             if (res.statusCode === 200) {
                 res.on('data', function (data) {
-                    const json = data.toString(),
-                        result = JSON.parse(json);
-
-                    result.images.forEach(function (img) {
-                        const reg = /^\/\w+/;
-                        for (const key in img) {
-                            const value = img[key];
-                            if (reg.test(value)) {
-                                img[key] = options.origin + value;
-                            }
-                        }
-                    });
-
-                    resolve(result);
+                    const json = data.toString();
+                    const body = json.replace(/(\:\s*")\/(\w+)/g, `$1${url.origin}$2`);
+                    resolve({ statusCode, headers, body });
                 });
             } else {
-                reject(res);
+                const body = res.statusMessage;
+                reject({ statusCode, headers, body });
             }
-        }).on('error', reject);
+        }).on('error', function (error) {
+            reject({
+                statusCode: 500,
+                body: error.toString()
+            })
+        });
     });
 }
 
-exports.handler = function (event, context, callback) {
-    console.log(process.version);
-
-    getData(event.queryStringParameters, { headers: event.headers }).then(function (res) {
-        send(null, 200, res);
-    }, function (error) {
-        send(error, error.statusCode || 500, error);
-    });
-
-    function send(error, statusCode, data) {
-        callback(error, {
-            statusCode,
-            body: JSON.stringify(data),
-            headers: {
-                'Content-Type': 'application/json; charset=utf-8',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Credentials': 'true',
-                'Access-Control-Allow-Headers': 'authorization'
-            }
-        });
-    }
+exports.handler = async function (event) {
+    return getData(
+        Object.assign(event.body, event.queryStringParameters),
+        { headers: event.headers }
+    );
 }
