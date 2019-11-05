94d62e71492dac1aa8970600ee257b1a3fba0028
zmy <ikoz@qq.com>
2019-11-05 17:04:02 +0800

    美化首页

--- README.md
+++ README.md
@@ -2,35 +2,39 @@
 
 以支持跨域的方式使用Bing每日壁纸API。
 
-## URL
+## 完整接口
+
+返回原汁原味的Bing壁纸数据。
+
+### URL
 
 https://bing-wallpaper.netlify.com/.netlify/functions/archive
 
-## 方法
+### 方法
 
 GET
 
-## 参数
+### 参数
 
 | 属性 | 类型 | 说明 | 默认值 |
 | - | - | - | - |
-| n | Number | 图片数量；最小为`1`，最大为`8` | `1` |
-| idx | Number | `0`为当天，大于0为历史图片，最大为`16` | `0` |
+| n | Number | 图片数量；最小为`1`，本项目最大为`4` | `1` |
+| idx | Number | `0`为当天，大于0为历史图片 | `0` |
 | format | String | 响应格式；可选：`js`、`xml`、`rss` | `js` |
 | mkt | String | 语言、区域，例如：`zh-CN` | `en-US` |
 | ensearch | Number | 详情模式，`0`关闭，`1`开启 | `0` |
 
-## 响应
+### 响应
 
 最外层为对象，有以下属性：
 
 | 属性 | 类型 | 说明 |
 | - | - | - |
 | base | String | 图片相对地址的基准地址。这是唯一本项目新增的属性，其他属性为原版接口返回值。只当`format=js`时存在，其他情况自行添加`https://www.bing.com`。 |
-| images | Array | 图片列表，每个元素为**图片对象** |
+| images | Array | 图片列表，每个元素为**图片信息对象** |
 | tooltips | Object | 工具提示，自行探索。 |
 
-### 图片对象
+#### 图片信息对象
 
 | 属性 | 类型 | 说明 |
 | - | - | - |
@@ -44,7 +48,7 @@ GET
 
 其他自行探索。
 
-## 响应示例
+### 响应示例
 
 ```json
 {
@@ -76,4 +80,40 @@ GET
     },
     "base": "https://www.bing.com"
 }
+```
+
+## 精简版接口
+
+返回优化可读性的图像列表。如果只需要获取图片和少量关键信息，这个接口更方便。
+
+### URL
+
+https://bing-wallpaper.netlify.com/.netlify/functions/simple
+
+### 参数
+
+跟完整接口一样，但是固定`format=js`。
+
+### 响应
+
+直接返回图片列表，每个元素为图片信息对象，对象具备下面字段：
+
+| 属性 | 类型 | 说明 |
+| - | - | - |
+| href | String | 图片绝对地址 |
+| title | String | 图片标题 |
+| color | String | 图片深（dark）浅（light）色 |
+| date | String | 图片日期 |
+
+### 响应示例
+
+```json
+[
+    {
+        "title": "Watch your step",
+        "href": "https://www.bing.com/th?id=OHR.TollymoreForest_EN-US2804177894_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp",
+        "date": "20191105",
+        "color": "dark"
+    }
+]
 ```
--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import * as api from '../../utils/api';
 
+const fnBase = 'https://bing-wallpaper.netlify.com/.netlify/functions',
+    bing = 'https://www.bing.com';
+
 class Home extends React.Component {
     state = {
         images: [],
@@ -11,34 +14,41 @@ class Home extends React.Component {
         const { state } = this;
 
         const image = state.images[state.index];
-        let style = {}, bgColor = 'bg-light';
+        let style = {}, bgColor = 'bg-dark';
 
         if (image) {
-            style.backgroundImage = `url(https://www.bing.com${image.url})`;
-            if (image.drk === 1)
-                bgColor = 'bg-dark';
+            style.backgroundImage = `url(${bing}${image.url})`;
+            if (image.drk !== 1)
+                bgColor = 'bg-light';
         }
 
         return (
             <div className={`view-home ${bgColor}`} style={style}>
-                <header>
+                <main className="masking">
                     <h1>Bing每日壁纸</h1>
-                </header>
-                <main>
                     <h2>以支持跨域的方式使用Bing每日壁纸API。</h2>
+                    <h3>
+                        <a href="https://github.com/kobezhu/bing-wallpaper#readme">文档（GitHub）</a>
+                    </h3>
                     <p>
                         完整版：
-                        <a href="/archive">https://bing-wallpaper.netlify.com/.netlify/functions/archive</a>
+                        <a href="/archive">{fnBase}/archive</a>
                     </p>
                     <p>
                         精简版：
-                        <a href="/simple">https://bing-wallpaper.netlify.com/.netlify/functions/simple</a>
-                    </p>
-                    <p>
-                        <a href="https://github.com/kobezhu/bing-wallpaper#readme">文档</a>
+                        <a href="/simple">{fnBase}/simple</a>
                     </p>
                 </main>
-                <footer>ZMY</footer>
+                <footer className="masking">
+                    <p>
+                        网站托管自<a href="https://www.netlify.com/">Netlify</a>，接口所有数据来自<a href={bing}>Microsoft Bing</a>，本站只增加跨域支持。
+                    </p>
+                    <p className="logo">
+                        <img src="https://cn.bing.com/sa/simg/bing_p_rr_teal_min.ico" alt="Bing-logo" />
+                        <img src="https://www.netlify.com/img/global/favicon/favicon-32x32.png" alt="Netlify-logo" />
+                    </p>
+                    <p>© {new Date().getFullYear()} ZMY</p>
+                </footer>
             </div>
         );
     }
